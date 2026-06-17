b78b19df4b3e89709aab75bfa510e84e2d500752
Alirie Gray <alirie.gray@gmail.com>
2026-06-17 13:26:04 -0700

    Add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, defaultSystem, Flex } from '@chakra-ui/react'
+import Login from "./pages/Login"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ChakraProvider value={defaultSystem}>
+        <Flex
+            minHeight={'100vh'}
+            flexDirection={'column'}
+            alignItems={'center'}
+            justifyContent={'center'}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <Login></Login>
+        </Flex>
+    </ChakraProvider>
   );
 }
 
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,30 @@
+﻿import { Field, Input, Flex, Text } from "@chakra-ui/react"
+import {
+    PasswordInput,
+} from "../components/ui/password-input"
+import { Link } from 'react-router'
+import { useState } from "react"
+
+function Login() {
+    const [password, setPassword] = useState("")
+    const [username, setUsername] = useState("")
+    
+    return (
+        <Flex width={500} flexDirection="column" justifyContent={'center'} alignItems={'center'} paddingBottom={'100px'}>
+            <Text marginBottom={25} fontSize={36}>Login</Text>
+            <Field.Root invalid>
+                <Field.Label>Username</Field.Label>
+                <Input value={username} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your username" />
+                <Field.ErrorText>This field is required</Field.ErrorText>
+            </Field.Root>
+
+            <Field.Root invalid>
+                <Field.Label>Password</Field.Label>
+                <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} />
+            </Field.Root>
+        <Text>No account yet? <Link to='/signup'>Sign up here!</Link> </Text>
+        </Flex>
+    );
+}
+
+export default Login;
